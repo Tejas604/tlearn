ff16a355d0ae2104bd72400a354bb9a6c04765a8
Tejas604 <125460276+Tejas604@users.noreply.github.com>
2024-10-25 21:38:40 +0000

    container config

---
+++ learn/test.js
@@ -0,0 +1,17 @@
+"use strict";  // use the code as new  js syntax
+
+console.log("tejas");
+const accountID = 12 ;
+let name = 'star';
+var lname = 'lord';
+
+console.table([accountID,name,lname]);
+// datatype
+//number
+/// null standalone value 
+/// undefined  the value has notassigned
+// symbol => unique mostly used in react
+
+///object
+
+console.log( typeof name);
