233598ef05d1ad53a1f73c1dad5bbd78ce31b812
Tejas604 <125460276+Tejas604@users.noreply.github.com>
2024-10-27 16:19:43 +0000

    commit

---
+++ learn/2.js
@@ -0,0 +1,39 @@
+ 
+//  ///// conversion of the data type
+ 
+//  let score = 33
+// console.log( typeof score);
+// let scar = "pain123"
+// console.log(scar); 
+
+// let valueInString = String(score);
+// console.log(score); /// convert number to string to the number
+
+// console.log( typeof valueInString);
+// let pain = Number(scar);
+// console.log(typeof pain);
+// console.log(pain);
+
+/// "33abc" => Nan
+/// null = 0
+//for bool
+/// 1 = true, 0  = false  ,  if blank  " " = false ," tejas" = true
+
+
+//////////operation////
+let val = 3
+let neg = -val;
+// console.log(val);
+// console.log(neg);
+//// 2+2 =4  2*2 = 4 2-2= 0 2**3  = 8 2/2 =0 2%5 = 2
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+ console.log("2" > 1);
+ console.log("02" > 1);
