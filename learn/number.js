12c3a7ae43f4798223ca4ea0de3cd488ab309063
Tejas604 <125460276+Tejas604@users.noreply.github.com>
2024-10-28 13:45:24 +0000

    maths

---
+++ learn/number.js
@@ -0,0 +1,41 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+//++++++++++++++++++++++++++maths++++++++++++++++++++++
+
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// // console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+
+
+
+
+// console.log(Math);
+// console.log(Math.random());
+// /// math.random gives value between 0 and 1 so we multiple it by 10 to get the value
+// console.log(((Math.random())  * 10)  + 1 ); /// we do the plus one cuz we don't want the value to be zero 
+// console.log(Math.floor((((Math.random())  * 10)  + 1 ))); 
+// we use the floor function to get single value instead of decimal value run this you will understand what i am talking about
+
+
+const min = 1
+const max = 6
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min) //// this the formula to genrate teh range we want the number to be in range example for dice game this formula is crucial
