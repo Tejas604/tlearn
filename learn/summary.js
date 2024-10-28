9cccc58610fe17426533dfdfeab83e9df46ad176
Tejas604 <125460276+Tejas604@users.noreply.github.com>
2024-10-28 08:23:42 +0000

    commit

--- learn/summary.js
+++ learn/summary.js
@@ -1,19 +1,19 @@
-//  Primitive
+// //  Primitive
 
-//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+// //  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
 
 
-const score = 100
-const scoreValue = 100.3
+// const score = 100
+// const scoreValue = 100.3
 
-const isLoggedIn = false
-const outsideTemp = null
-let userEmail;
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 // const bigNumber = 3456543576654356754n
 
@@ -22,16 +22,27 @@ console.log(id === anotherId);
 // Reference (Non primitive)  usually its datatype is a object and for function it is also a objectfunctiom
 // Array, Objects, Functions
 
-const heros = ["shaktiman", "naagraj", "doga"];
+// const heros = ["shaktiman", "naagraj", "doga"];
 let myObj = {
     name: "hitesh",
     age: 22,
 }
 
-const myFunction = function(){
-    console.log("Hello world");
-}
+// const myFunction = function(){
+//     console.log("Hello world");
+// }
 
-console.log(typeof anotherId);
+// console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+  /////////////////memeory//////////
+   // stack(primitive) copy of the memory, heap(non-primitive) reference of the memeory
+
+   let ob2 = myObj
+   ob2.name = "tejas";
+   console.log(myObj);
+   
+   console.log(ob2) ;
