e7bb90638d8f4da520a0e848f7c51e4445282a7a
Tejas604 <125460276+Tejas604@users.noreply.github.com>
2024-10-30 20:21:48 +0000

    function

---
+++ learn/function.js
@@ -0,0 +1,34 @@
+function calloutmyname() {
+    console.log("tejas");
+    console.log("tejas");
+    console.log("tejas");
+    console.log("tejas");
+    
+    
+}
+// calloutmyname()
+
+// function addtwonumber (number1 , number2){
+//     console.log(number1 + number2);
+    
+// }
+
+function addtwonumber (number1 , number2){
+// let result = number1 + number2
+// return result
+return number1 + number2
+    
+}
+const result = addtwonumber(1,1);
+console.log(result);
+
+function login(username){
+    return `${username} just loggedin`
+}
+
+console.log(login("tejas"));
+
+
+
+
+
