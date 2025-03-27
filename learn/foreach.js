c3299544a6d1c02d5e6765095d54fca99cf0aff7
Tejas604 <125460276+Tejas604@users.noreply.github.com>
2025-03-27 12:00:10 +0000

    await

--- learn/foreach.js
+++ learn/foreach.js
@@ -1,5 +1,5 @@
 
-var arr = [1,2,3,4];
+var arr = [1, 2, 3, 4];
 
 // arr.forEach(function(val){
 //     console.log(val + "2" + " hello")
@@ -69,3 +69,26 @@ var obj = {
 
 // so we can evee find the function length since function is object at  the core 
 // to find the length function name.length same how we use to find for array
+
+
+/// async function
+
+
+//// so first question that popin your mind is what is async or what is sync right 
+////  so first what is sync is something which run the code  by line by line that is called as synchronize function
+
+// now what is aysnc function
+// see the aysnc  function jo bhi code aync nature ka hota hai usko  side stack mein bhej do jo bhi code sync nature ka ho woh unk run kar do fir jab sare sync khatam ho jaye tab check karo if aysnc completehua kya hua toh unko main meh leka run karo
+
+
+// async function abcd() {
+//    var blob  = await fetch('https://randomuser.me/api/');
+//     var ans  = await blob.json();
+
+//     console.log(ans);
+    
+// }
+// abcd();
+
+
+// we have to right so that the daṭa is waiting ready to be execu6ted 
